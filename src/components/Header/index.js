fbea0d1c5d70f1f44f9c3b971fa9fd19eafd0499
laura-2 <laurahelenaverbinen@gmail.com>
2023-06-20 18:03:01 -0300

    navBar responsivo

---
+++ src/components/Header/index.js
@@ -0,0 +1,68 @@
+import React , {useState} from "react";
+import LogoDesktop from "../../assets/Desktop/Logo desktop.png"
+import Menu from "../../assets/Mobile/Ícones/Icon Color.png"
+import Close from "../../assets/Mobile/Ícones/marca-cruzada 1.png"
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import "./header.css"
+export default function Header(){
+    const [menu, setMenu] = useState(false);
+    const menuOpen = () => {
+        setMenu(!menu)
+    }
+    
+    return (
+        <>
+            <Navbar className="header">
+            <Container fluid>
+                <Image src={LogoDesktop}/>
+                <Nav
+                    className="me-auto my-2 my-lg-0 header-tablet"
+                >
+                    <Nav.Link href="#action1" className="header-link">Home</Nav.Link>
+                    <Nav.Link href="#action2" className="header-link">Nossas lojas</Nav.Link>
+                    <Nav.Link href="#action2" className="header-link">Novidades</Nav.Link>
+                    <Nav.Link href="#action2" className="header-link">Promoções</Nav.Link>
+                </Nav>
+                <Image src={Menu} onClick={menuOpen} className="menu-open"/>
+                {menu && (
+                    <Nav
+                        className="header-mobile header-menu-links"
+                    >
+                        <Image src={Close} className="header-menu-close" onClick={menuOpen}/>
+                        <Nav.Link href="#action1" className="header-link link-color">Home</Nav.Link>
+                        <hr className="header-link-color"/>
+                        <Nav.Link href="#action2" className="header-link">Nossas lojas</Nav.Link>
+                        <hr className="header-link-color"/>
+                        <Nav.Link href="#action2" className="header-link">Novidades</Nav.Link>
+                        <hr className="header-link-color"/>
+                        <Nav.Link href="#action2" className="header-link">Promoções</Nav.Link>
+                    </Nav>
+                )}
+                <Form className="d-flex header-search">
+                    <Form.Control
+                    type="search"
+                    placeholder="Digite o produto"
+                    className="me-2 header-input"
+                    aria-label="Search"
+                    />
+                    <Button className="header-button" variant="dark">Buscar</Button>
+                </Form>
+            </Container>
+            </Navbar>
+            <Form className="d-flex header-search-mobile">
+                    <Form.Control
+                    type="search"
+                    placeholder="Digite o produto"
+                    className="me-2 header-input-mobile"
+                    aria-label="Search"
+                    />
+                    <Button className="header-button-mobile" variant="dark">Buscar</Button>
+                </Form>
+            </>
+    )
+}
