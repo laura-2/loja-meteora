27d3fba3c8bfee0f1cac4b0eefd029aae5cfac0c
laura-2 <laurahelenaverbinen@gmail.com>
2023-06-27 12:56:56 -0300

    modal mobile and tablet

--- src/components/CardPrice/index.js
+++ src/components/CardPrice/index.js
@@ -1,25 +1,20 @@
 import React, {useState} from "react"
 import Card from 'react-bootstrap/Card';
 import {clothes} from '../../data'
-import ModalShowDetails from "../../action/ModalShowDetails";
 import check from "../../assets/Desktop/Ícones/check-circle.png"
-import { Row, Col, Form, Modal, CloseButton } from 'react-bootstrap';
+import close from "../../assets/Desktop/Ícones/Icon Color.png"
+import { Row, Col, Form, Modal} from 'react-bootstrap';
 import "./cardPrice.css"
 export default function CardPrice (){
   const [modalShow, setModalShow] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
-
-  // const imageCard = clothes.find((clothes) => clothes.id === selectedCard).imagem;
-  // const titleCard = clothes.find((clothes) => clothes.id === selectedCard).title;
-  // const textCard = clothes.find((clothes) => clothes.id === selectedCard).text;
-  // const priceCard = clothes.find((clothes) => clothes.id === selectedCard).price;
 
   const handleOpen = (cardId) => {
       setSelectedCard(cardId);
       setModalShow(true)
   };
   const closeModal = () => {
-    setModalShow(false);
+    setModalShow(modalShow);
   };
   const listModel = clothes.map(clothes => 
     <div key={clothes.id}>
@@ -47,19 +42,75 @@ export default function CardPrice (){
         {listModel}
       </Row>
       {selectedCard && (
-        
+        <>
         <div className="modal">
           <div className="modal-content">
-         <h3>{clothes.title}</h3>
-          <p>oii</p>
-          <p>{clothes.find((clothes) => clothes.id === selectedCard).title}</p>
-          <button onClick={() => setModalShow(false)}>Close</button>
+          <Modal.Header className="modal-header">
+            <img src={check} alt='Icon check' className="modal-icon"/>
+            <Modal.Title id="contained-modal-title-left" className="modal-title">
+            Confira detalhes sobre o produto
+            </Modal.Title>
+            <img src={close} alt="Icon close" onClick={closeModal}/>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="modal-flex">
+              <div>
+                <img src={clothes.find((clothes) => clothes.id === selectedCard).imagem} alt='imagem-modelo' className="modal-image"/>
+              </div>
+              <div>
+                  <Modal.Title className="modal-title-price">{clothes.find((clothes) => clothes.id === selectedCard).title}</Modal.Title>
+                  <p className="modal-text">{clothes.find((clothes) => clothes.id === selectedCard).text}</p>
+                  <hr/>
+                  <Modal.Title className="modal-title-price">{clothes.find((clothes) => clothes.id === selectedCard).price}</Modal.Title>
+                  <p className="modal-text-light">Vendido e entregue por Riachuelo</p>
+                  <hr/>
+                  <p className="modal-text-color">Cores:</p>
+                    <Form>
+                    <label className="modal-label">
+                    <input type="radio" name="myRadio" value="option1" className="modal-input"/>
+                    Azul claro
+                  </label>
+                  <label className="modal-label">
+                    <input type="radio" name="myRadio" value="option1" className="modal-input" />
+                    Offwhite
+                  </label>
+                  <label className="modal-label">
+                    <input type="radio" name="myRadio" value="option1" className="modal-input" />
+                    Preto
+                  </label>
+                    </Form>
+                    <hr/>
+                    <p className="modal-text-color">Tamanho:</p>
+                    <Form>
+                      <label className="modal-label">
+                        <input type="radio" name="myRadio" value="option1" className="modal-input" />
+                        PP 
+                      </label>
+                      <label className="modal-label">
+                        <input type="radio" name="myRadio" value="option1" className="modal-input" />
+                        P
+                      </label>
+                      <label className="modal-label">
+                        <input type="radio" name="myRadio" value="option1" className="modal-input" />
+                        M
+                      </label>
+                      <label className="modal-label">
+                      <input type="radio" name="myRadio" value="option1" className="modal-input" />
+                        G
+                      </label>
+                      <label className="modal-label">
+                        <input type="radio" name="myRadio" value="option1" className="modal-input" />
+                        GG
+                      </label>
+                    </Form>
+
+                    <button className="modal-button">Adicionar à sacola</button>
+                </div>
+              </div>
+            </Modal.Body>
          </div>
          </div>
-      
-      // <ModalShowDetails
-      // show={modalShow}   
-      // onHide={() => setModalShow(false)} />
+      </>
       )}
     </>
     
